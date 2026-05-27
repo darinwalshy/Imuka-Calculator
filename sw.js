bc715e2ff5824ca8d661ba8824ad489c3c1bea3e
darinwalshy <darinwalshy@gmail.com>
2026-05-27 17:37:42 +0300

    Update sw.js
    Hopefully resolving the cache'ing issue by putting the network (online) version first, instead of the cache

--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
-const CACHE_NAME = 'imuka-v2.2';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'imuka-v4'; // Your version bump
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  // Add other assets like icons here
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+// Install event
+self.addEventListener('install', (event) => {
+  // Forces the waiting service worker to become the active service worker
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Activate event - Cleans up old caches automatically
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event: NETWORK FIRST strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
